228ec8be9cd2233991caf0b2b841ade635e8067e
person973 <ddw6891@g.rit.edu>
2025-10-11 20:55:06 -0400

    Finished AudioManager for now

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,134 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+/// <summary>
+/// The audio system for Dam-ed.
+/// </summary>
+public class AudioSystem : MonoBehaviour
+{
+    //Fields
+    /// <summary>
+    /// A number that determines what track/sound plays when.
+    /// </summary>
+    [SerializeField]
+    [Tooltip ("A number that determines what track/sound plays when.")]
+    private float _nextAudio;
+
+    /// <summary>
+    /// The amound of time since the game has been opened, in seconds
+    /// </summary>
+    [SerializeField]
+    [Tooltip("The amound of time since the game has been opened, in seconds")]
+    private float _audioTime;
+
+    /// <summary>
+    /// A reference to the BeaverManager gameObject.
+    /// </summary>
+    [SerializeField]
+    [Tooltip ("A reference to the BeaverManager gameObject.")]
+    private GameObject _beaverManager;
+
+    /// <summary>
+    /// The number of beavers left the players know about.
+    /// </summary>
+    [SerializeField]
+    [Tooltip ("The number of beavers left the players know about.")]
+    private float _knownBeaversLeft;
+
+    /// <summary>
+    /// Jumpscare-ish tracks such as panic1-4
+    /// </summary>
+    [SerializeField]
+    [Tooltip ("Jumpscare-ish tracks such as panic1-4.")]
+    private List<AudioClip> _jumpscareMus;
+
+    /// <summary>
+    /// Regular music that plays during gameplay.
+    /// </summary>
+    [SerializeField]
+    [Tooltip ("Regular music that plays during gameplay.")]
+    private List<AudioClip> _normalMus;
+
+    /// <summary>
+    /// Ambient sounds that play during gamplay.
+    /// </summary>
+    [SerializeField]
+    [Tooltip ("Ambient sounds that play during gamplay.")]
+    private List<AudioClip> _ambientSounds;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GetNextAudioTime();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Updating _knownBeaversLeft each frame
+        //Should probably change this to whenever the _knownBeaversLeft changes, but meh
+        //_knownBeaversLeft = _beaverManager.GetComponent<BeaverManager>()._knownBeaversLeft;
+
+        AdvanceAudio(Time.deltaTime);
+    }
+
+    /// <summary>
+    /// Advances the 
+    /// </summary>
+    /// <param name="time"></param>
+    public void AdvanceAudio(float time)
+    {
+        _audioTime += time;
+        PlaySound();
+        GetNextAudioTime();
+    }
+
+    /// <summary>
+    /// Play an ambient track/noise if conditions are met.
+    /// </summary>
+    public void PlaySound()
+    {
+        AudioSource aS = gameObject.GetComponent<AudioSource>();
+
+        //Checks if enough time has passed and a sound isn't already being played.
+        if (_audioTime >= _nextAudio && !aS.isPlaying)
+        {
+            _audioTime -= _nextAudio;
+            //Roll 1d3.
+            //1-Plays a random track from panic1-4
+            //2-Plays a random regular track
+            //3-Plays an ambient sound
+            int die = Random.Range(1, 4);
+            int index = 0;
+
+            switch (die)
+            {
+                case 1:
+                    index = Random.Range(0, _jumpscareMus.Count);
+                    aS.clip = _jumpscareMus[index];
+                    break;
+
+                case 2:
+                    index = Random.Range(0, _normalMus.Count);
+                    aS.clip = _normalMus[index];
+                    break;
+
+                case 3:
+                    index = Random.Range(0, _ambientSounds.Count);
+                    aS.clip = _ambientSounds[index];
+                    break;
+            }
+
+            aS.Play();
+        }
+    }
+
+    /// <summary>
+    /// Gets the next audio time.
+    /// </summary>
+    public void GetNextAudioTime()
+    {
+        //A random amount of time, from 0-15 minutes, in seconds.
+        _nextAudio = Random.Range(0f, 900f);
+    }
+}
